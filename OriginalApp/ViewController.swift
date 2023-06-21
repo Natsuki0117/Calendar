2eb212a4e03b92789820a07c488ea68e30c0d2db
NATSUKI <natsu000117@icloud.com>
2023-06-21 18:47:37 +0900

    Initial Commit

---
+++ OriginalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OriginalApp
+//
+//  Created by 金井菜津希 on 2023/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
