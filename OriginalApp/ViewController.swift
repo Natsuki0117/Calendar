39cfc745982704c8091808a0ee480be2e6051ab4
tsuyuni <tsuyu.lit@gmail.com>
2023-08-05 11:45:56 +0900

    ViewControllerを修正

--- OriginalApp/ViewController.swift
+++ OriginalApp/ViewController.swift
@@ -9,17 +9,7 @@ import UIKit
 import RealmSwift
 import FSCalendar
 
-class ViewController: UIViewController, UITableViewDataSource, FSCalendarDataSource,FSCalendarDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-   
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FSCalendarDataSource,FSCalendarDelegate {
     
     @IBOutlet var tableview: UITableView!
     @IBOutlet var calendar: FSCalendar!
@@ -35,6 +25,7 @@ class ViewController: UIViewController, UITableViewDataSource, FSCalendarDataSou
         // Do any additional setup after loading the view.
         
         tableview.dataSource = self
+        tableview.delegate = self
         tableview.register(UINib(nibName: "ItemTableViewCell", bundle: nil), forCellReuseIdentifier: "ItemCell")
         items = readItems()
         
@@ -43,105 +34,85 @@ class ViewController: UIViewController, UITableViewDataSource, FSCalendarDataSou
         
         let test = calendar
         self.calendar
+        // 全件検索します。
+        let results = realm.objects(item.self)
         
-        do {
+        // 検索結果の件数を取得します。
+        let count = results.count
+        if (count == 0) {
+            // 検索データ0件の場合
             
             
-            // 全件検索します。
-            let results = realm.objects(item.self)
+        } else {
+            // 検索データがある場合
             
-            // 検索結果の件数を取得します。
-            let count = results.count
-            if (count == 0) {
-                // 検索データ0件の場合
+            // コレクションとしてアクセスする場合
+            // resultは"item"クラスとしてアクセスできます。
+            for result in results {
+                print("\(String(describing: result.isSameObject(as: )))")
                 
-                
-            }else {
-                // 検索データがある場合
-                
-                // コレクションとしてアクセスする場合
-                // resultは"item"クラスとしてアクセスできます。
-                for result in results {
-                    print("\(String(describing: result.isSameObject(as: )))")
-                    
-                    
-                }
-                
-                // インデックスを指定してアクセスする場合
-                // results[i]は"item"クラスとしてアクセスできます。
-                for i in 0 ..< count {
-                    print("\(String(describing: results[i].isSameObject(as: )))")
-                }
-
-                    //                エラー処理必要
-                func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-                    self.date = date
-                }
-                
-            } catch {
                 
             }
             
-            
-        }
-        
-        
-        
-        func viewWillAppear(_ animated: Bool) {
-            items = readItems()
-            tableview.reloadData()
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return items.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
-            
-            let item: item = items[indexPath.row]
-            
-            cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
-            
-            
-            return cell
-            
-        }
-        
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath as IndexPath) as! ItemTableViewCell
-            let _: item = items[indexPath.row]
-            //            cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
-            
-            return cell
-            
-        }
-        
-        func readItems() -> [item] {
-            return Array(realm.objects(item.self))
-        }
-        
-        internal func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                //             realmのデータを更新
-                try! realm.write {
-                    let test = items.remove(at: indexPath.row)
-                    realm.delete(test)
-                    tableView.deleteRows(at: [indexPath], with: .automatic)
-                }
+            // インデックスを指定してアクセスする場合
+            // results[i]は"item"クラスとしてアクセスできます。
+            for i in 0 ..< count {
+                print("\(String(describing: results[i].isSameObject(as: )))")
             }
-        }
-        
-        func addItem(){
-            let nextVC = storyboard?.instantiateViewController(withIdentifier: "NewItem") as! NextViewItemController
-            nextVC.date = date
-            self.present(nextVC, animated: true)
             
+            //                エラー処理必要
         }
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        self.date = date
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        items = readItems()
+        tableview.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
+        let item: item = items[indexPath.row]
+        cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath as IndexPath) as! ItemTableViewCell
+        let _: item = items[indexPath.row]
+        //            cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
         
+        return cell
         
     }
+    
+    func readItems() -> [item] {
+        return Array(realm.objects(item.self))
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            //             realmのデータを更新
+            try! realm.write {
+                let test = items.remove(at: indexPath.row)
+                realm.delete(test)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        }
+    }
+    
+    func addItem(){
+        let nextVC = storyboard?.instantiateViewController(withIdentifier: "NewItem") as! NextViewItemController
+        nextVC.date = date
+        self.present(nextVC, animated: true)
+        
+    }
+}
