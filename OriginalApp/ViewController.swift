4ddf4e0d93de8b7eafd764a969ae802c2dd03b19
tsuyuni <tsuyu.lit@gmail.com>
2023-08-08 23:08:32 +0900

    コード修正

--- OriginalApp/ViewController.swift
+++ OriginalApp/ViewController.swift
@@ -8,81 +8,34 @@ import UIKit
 import RealmSwift
 import FSCalendar
 
-class ViewController: UIViewController, UITableViewDataSource, FSCalendarDataSource,FSCalendarDelegate {
-    
-    @IBAction func addItem(){
-        let nextVC = storyboard?.instantiateViewController(withIdentifier: "NewItem") as! NextViewItemController
-        nextVC.date = self.date
-        self.present(nextVC, animated: true, completion: nil)
-        
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
-        
-        let item: item = items[indexPath.row]
-        
-        cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
-        
-        
-        return cell
-        
-    }
-    
-    
-    
-    
-    
-    
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet var tableview: UITableView!
     @IBOutlet var calendar: FSCalendar!
     
-    var realm = try! Realm()
-    var items: [item] = []
-    var label:UILabel!
     let df = DateFormatter()
-    let fscCalendar = FSCalendar()
+    let realm = try! Realm()
     
+    var items: [item] = []
     var date: Date!
     
-    
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        df.dateFormat = "yyyy/MM/dd"
-        label.text = df.string(from: date)
-        self.date = date
+    // 画面表示前に実行される処理
+    override func viewWillAppear(_ animated: Bool) {
+        items = readItems()
+        tableview.reloadData()
     }
     
-    
-    
+    // 画面表示時に実行される処理
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        
-        
-        tableview.dataSource = self
         tableview.register(UINib(nibName: "ItemTableViewCell", bundle: nil), forCellReuseIdentifier: "ItemCell")
         items = readItems()
-        
-        self.calendar.dataSource = self
-        self.calendar.delegate = self
-        
-        
-        _ = calendar
-        self.calendar
-        
         let results = realm.objects(item.self)
-        
         let count = results.count
+        
         if (count == 0) {
             
         } else {
-            
             for result in results {
                 print("\(String(describing: result.isSameObject(as: )))")
             }
@@ -91,58 +44,50 @@ class ViewController: UIViewController, UITableViewDataSource, FSCalendarDataSou
                 print("\(String(describing: results[i].isSameObject(as: )))")
             }
         }
+    }
+    
+    // テーブルビューの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    // テーブルビューの各行に表示する内容
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-       
-        
-        func viewWillAppear(_ animated: Bool) {
-            items = readItems()
-            tableview.reloadData()
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return items.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
-            
-            let item: item = items[indexPath.row]
-            
-            cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
-            
-            
-            return cell
-            
-        }
-        
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath as IndexPath) as! ItemTableViewCell
-            let _: item = items[indexPath.row]
-            //            cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
-            
-            return cell
-            
-        }
-        
-        func readItems() -> [item] {
-            return Array(realm.objects(item.self))
-        }
-        
-        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-                //             realmのデータを更新
-                try! realm.write {
-                    let test = items.remove(at: indexPath.row)
-                    realm.delete(test)
-                    tableView.deleteRows(at: [indexPath], with: .automatic)
-                }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemTableViewCell
+        let item = items[indexPath.row]
+        cell.setCell(title: item.title, date: item.date, isMarked: item.isMarked)
+        return cell
+    }
+    
+    // Realmからデータを取得
+    func readItems() -> [item] {
+        return Array(realm.objects(item.self))
+    }
+    
+    // テーブルビューのアクションを設定
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Realmのデータを更新
+            try! realm.write {
+                let test = items.remove(at: indexPath.row)
+                realm.delete(test)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
             }
         }
-        
-        
-        
-        
+    }
+    
+    // アイテム追加ボタン押下時の処理(画面遷移)
+    @IBAction func addItem(){
+        let nextVC = storyboard?.instantiateViewController(withIdentifier: "NewItem") as! NextViewItemController
+        nextVC.date = self.date
+        self.present(nextVC, animated: true, completion: nil)
+    }
+}
+
+// 以下はFSCalendar関連のクラス拡張
+extension ViewController: FSCalendarDataSource, FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        self.date = date
     }
 }
